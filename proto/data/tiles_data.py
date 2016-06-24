c682ee109117f7ae7b80aa29f99fdf7d86d260f1
Sergii Rustamov <sergii.rustamov@gmail.com>
2016-06-24 22:20:13 +0300

    wip

--- proto/data/tiles_data.py
+++ proto/data/tiles_data.py
@@ -1,8 +1,8 @@
 import pygame
 
-TILES = {'player': pygame.Rect(1 * 32, 2 * 32, 32, 32),
-         'predator_01': pygame.Rect(1 * 32, 3 * 32, 32, 32),
-         'test_item_01': pygame.Rect(1 * 32, 4 * 32, 32, 32),
-         'test_item_02': pygame.Rect(1 * 32, 8 * 32, 32, 32),
-         'test_item_03': pygame.Rect(1 * 32, 10 * 32, 32, 32),
-         'hand_item': pygame.Rect(1 * 32, 9 * 32, 32, 32)}
+TILES = {'player': pygame.Rect(3 * 32, 0 * 32, 32, 32),
+         'predator_01': pygame.Rect(0 * 32, 0 * 32, 32, 32),
+         'test_item_01': pygame.Rect(5 * 32, 0 * 32, 32, 32),
+         'test_item_02': pygame.Rect(1 * 32, 0 * 32, 32, 32),
+         'test_item_03': pygame.Rect(4 * 32, 0 * 32, 32, 32),
+         'hand_item': pygame.Rect(2 * 32, 0 * 32, 32, 32)}
