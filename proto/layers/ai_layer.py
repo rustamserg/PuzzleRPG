dce5778b10850e84e81d2d437efecc1b25437478
Sergii Rustamov <sergii.rustamov@gmail.com>
2016-06-27 22:21:07 +0300

    hunt wip

--- proto/layers/ai_layer.py
+++ proto/layers/ai_layer.py
@@ -9,14 +9,14 @@ from world import TurnType
 class AILayer(Layer):
     def __init__(self):
         Layer.__init__(self)
-        self.move_time = time.clock()
+        self.move_time = time.monotonic()
 
     def start(self, world):
         ground_layer = world.get_layer('GroundLayer')
         to_spawn = 10
         while to_spawn > 0:
-            row = random.randint(1, world.hex_map.height - 1)
-            column = random.randint(1, world.hex_map.width - 1)
+            row = random.randint(1, world.hex_map.height/4 - 1)
+            column = random.randint(1, world.hex_map.width/4 - 1)
             cell = world.hex_map.get_cell(row, column)
             if cell is not None:
                 if ground_layer.can_move_to_cell(cell):
@@ -31,11 +31,11 @@ class AILayer(Layer):
 
     def update(self, world, turn):
         if turn == TurnType.PLAYER:
-            self.move_time = time.clock()
+            self.move_time = time.monotonic()
             return
 
-        diff_sec = time.clock() - self.move_time
-        if diff_sec > 1:
+        diff_sec = time.monotonic() - self.move_time
+        if diff_sec > 0.3:
             for ent in self.entities:
                 ent.do_turn(world)
             world.end_turn()
